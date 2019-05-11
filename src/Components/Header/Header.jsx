68ba45c7480d986c9c510c020fc44d08f7c96979
shakti97 <shaktisingh230797@gmail.com>
2019-05-12 02:24:35 +0530

    Build Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+const Header=()=>{
+    return (
+        <React.Fragment>
+            <div className='header'>
+                Twitter App
+            </div>
+        </React.Fragment>
+    );
+
+}
+
+export default Header;
