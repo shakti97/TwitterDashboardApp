2ffed44095392c922e46203c1fe984a5af73a6dc
shakti97 <shaktisingh230797@gmail.com>
2019-05-12 02:25:25 +0530

    Refactor App

--- src/Components/FilterTweets/FilterTweets.jsx
+++ src/Components/FilterTweets/FilterTweets.jsx
@@ -6,15 +6,20 @@ const FilterTweets =(props)=>{
         <React.Fragment>
             <form>
                 <div className="form-group row">
-                    <div className="col-md-6">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-4">
                         <input type="text" placeholder='Search' name='searchValue' onChange={props.handleChange} className='form-control'/>
+                        {props.clientSideValidation.includes('tweetLength') && <div className='redColor'>Must A Number for Tweet Length Filter</div>}
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-md-4">
                         <select name="filterName" id="filterName" className='form-control' onChange={props.handleChange}>
-                            <option value="" defaultValue disabled>Select Filter Type</option>
+                            <option value="" defaultValue>Select Filter Type</option>
                             <option value="tweetLength">Tweet Length</option>
                             <option value="textSearch">Text Search</option>
                         </select>
+                    </div>
+                    <div className="col-md-1">
+                        <button className='btn btn-primary' onClick={props.searchTweets}>Search</button>
                     </div>
                 </div>
             </form>
--- src/Container/Main.jsx
+++ src/Container/Main.jsx
@@ -1,31 +1,95 @@
 import React, { Component } from 'react';
-import Twitter from 'twitter';
 import Axios from 'axios';
 import DisplayTweet from '../Components/DisplayTweets/DisplayTweet';
 import FilterTweets from '../Components/FilterTweets/FilterTweets';
+import Header from '../Components/Header/Header';
+import './Main.css';
 
+const regexNumber=/^[0-9]+$/;
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = { 
             userName : '',
+            searchValue : '',
+            filterName : '',
+            clientSideValidation : [], 
             tweets : [],
             filterTweets : []
         };
     }
     handleChange=(event)=>{
         event.preventDefault();
-        var targetName=event.target.name;
         this.setState({
             [event.target.name] : event.target.value
         },()=>{
-            if(targetName==='filterName'){
-
+            console.log(this.state);
+            if(this.state.searchValue===''){
+                this.setState({
+                    filterTweets : this.state.tweets
+                })
             }
+        })
+    }
+    searchTweets=(event)=>{
+        event.preventDefault();
+        console.log('search Tweet');
+        if(this.state.filterName==='tweetLength'){
+            if(!regexNumber.test(this.state.searchValue)){
+                this.setState({
+                    clientSideValidation : ['tweetLength']
+                })
+            }
+            else{
+                console.log('Calling filter By Length');
+                this.filterByLength(this.state.searchValue);
+            }
+        }
+        if(this.state.filterName==='textSearch'){
+            this.filterByText(this.state.searchValue);
+        }
+    }
+    filterByText=(searchedText)=>{
+        var filterTweets=this.state.tweets;
+        var filteredTweets=filterTweets.filter(tweet=>{
+            if((tweet.text).includes(searchedText)){
+                return tweet;
+            }
+        })
+        this.setState({
+            filterTweets : filteredTweets
+        },()=>{
+            console.log('filter By Text',this.state.filterTweets);
+        })
+    }
+    filterByLength=(tweetLength)=>{
+        var filterTweets=this.state.tweets;
+        var filteredTweets=filterTweets.filter((tweet)=>{
+            console.log("tweet Length "+tweet.length+" search tweet length "+tweetLength );
+            if(tweet.length==tweetLength){
+                return tweet;
+            }
+        })
+        console.log('filterTweet',filteredTweets);
+        this.setState({
+            filterTweets : filteredTweets
         })
     }
     componentDidMount(){
         this.getMyServerTweets();
+    }
+    getTweetsLength=()=>{
+        var tweetsArray=[];
+        this.state.tweets.forEach(tweet=>{
+            tweet.length=(tweet.text).length;
+            tweetsArray.push(tweet);
+        })
+        this.setState({
+            tweets : tweetsArray,
+            filterTweets : tweetsArray
+        },()=>{
+            console.log(this.state.tweets);
+        })
     }
     getMyServerTweets=(userName='Shakti2397')=>{
         Axios.get('http://localhost:8080/tweets/'+userName)
@@ -36,15 +100,19 @@ class Main extends Component {
                 filterTweets : response.data
             },()=>{
                 console.log(this.state.tweets);
+                this.getTweetsLength();
             })
         })
     }
     render() {
         return (
             <React.Fragment>
-                <div className="container">
-                    <FilterTweets searchValue={this.state.searchValue} filterName={this.state.filterName} handleChange={this.handleChange}/>
-                    <DisplayTweet tweets={this.state.filterTweets}/>
+                <div className="mainContainer">
+                    <Header/>
+                    <div className="container">
+                        <FilterTweets searchValue={this.state.searchValue} searchTweets={this.searchTweets} clientSideValidation={this.state.clientSideValidation} filterName={this.state.filterName} handleChange={this.handleChange}/>
+                        <DisplayTweet tweets={this.state.filterTweets}/>
+                    </div>
                 </div>
             </React.Fragment>
         );
