c4ccda552c76c41bbc524da2ed84293a8975ecd2
shakti97 <shaktisingh230797@gmail.com>
2019-05-11 23:56:02 +0530

    Refactor to get User Tweets

--- src/Container/Main.jsx
+++ src/Container/Main.jsx
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
 import Twitter from 'twitter';
+import Axios from 'axios';
+import DisplayTweet from '../Components/DisplayTweets/DisplayTweet';
+import FilterTweets from '../Components/FilterTweets/FilterTweets';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            userName : ''
+            userName : '',
+            tweets : [],
+            filterTweets : []
         };
     }
-    componentDidMount(){
-        this.getTweets();
+    handleChange=(event)=>{
+        event.preventDefault();
+        var targetName=event.target.name;
+        this.setState({
+            [event.target.name] : event.target.value
+        },()=>{
+            if(targetName==='filterName'){
+
+            }
+        })
     }
-    getTweets=()=>{
-        var client = new Twitter({
-            consumer_key: 'FNOvdUa4h5QCrwxJ4GhF9kRVG',
-            consumer_secret: 'J18izC0u1cFU653DUxLgedxwFBIAGd9kbocx6Doqf82OPRMwtj',
-            access_token_key: '',
-            access_token_secret: ''
-        });
-           
-        var params = {q: 'Shakti2397'};
-        client.get('search/tweets', params, function(error, tweets, response) {
-        if (!error) {
-            console.log(tweets);
-        }
-        });
+    componentDidMount(){
+        this.getMyServerTweets();
+    }
+    getMyServerTweets=(userName='Shakti2397')=>{
+        Axios.get('http://localhost:8080/tweets/'+userName)
+        .then(response=>{
+            console.log(response);
+            this.setState({
+                tweets : response.data,
+                filterTweets : response.data
+            },()=>{
+                console.log(this.state.tweets);
+            })
+        })
     }
     render() {
         return (
             <React.Fragment>
-
+                <div className="container">
+                    <FilterTweets searchValue={this.state.searchValue} filterName={this.state.filterName} handleChange={this.handleChange}/>
+                    <DisplayTweet tweets={this.state.filterTweets}/>
+                </div>
             </React.Fragment>
         );
     }
