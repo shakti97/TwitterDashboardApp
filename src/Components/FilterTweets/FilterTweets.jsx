8a91cdfdd6ffb2ae7c5368f04373b0491a408279
shakti97 <shaktisingh230797@gmail.com>
2019-05-11 23:57:28 +0530

    setUp Filter Fields

--- src/Components/FilterTweets/FilterTweets.jsx
+++ src/Components/FilterTweets/FilterTweets.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import './FilterTweets.css';
 
-class FilterTweets extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-
-        };
-    }
-    render() {
-        return (
-            <React.Fragment>
-                
-            </React.Fragment>
-        );
-    }
+const FilterTweets =(props)=>{
+    return (
+        <React.Fragment>
+            <form>
+                <div className="form-group row">
+                    <div className="col-md-6">
+                        <input type="text" placeholder='Search' name='searchValue' onChange={props.handleChange} className='form-control'/>
+                    </div>
+                    <div className="col-md-6">
+                        <select name="filterName" id="filterName" className='form-control' onChange={props.handleChange}>
+                            <option value="" defaultValue disabled>Select Filter Type</option>
+                            <option value="tweetLength">Tweet Length</option>
+                            <option value="textSearch">Text Search</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+        </React.Fragment>
+    );
 }
 
 export default FilterTweets;
